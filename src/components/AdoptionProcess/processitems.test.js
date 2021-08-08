291f37eaa59dc2f5448a46eb7d49b7650d166726
ayse8888 <aysebasar@yahoo.com>
2021-08-08 21:09:37 +0300

    Build: add Pet Adoption Process Component

---
+++ src/components/AdoptionProcess/processitems.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ProcessItems from './ProcessItems.js';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<ProcessItems />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
