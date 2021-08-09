f1eeb896587b3829256239d1170ef5a5b3922429
mustafaguldag <guldag.mustafaa@gmail.com>
2021-08-09 10:32:27 +0300

    Test: fixed the test

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,25 +1,29 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import logo from "../../images/buttonlogo.png";
-import "./HeroSection.css"
+import "./HeroSection.css";
 
-const HeroSection = ({img, title, text}) => {
-    
-    const background ={
-            backgroundImage: `url(${img})`,
-    };
-      
-    return(
-        <Container className="container" style={background}>
-          <Row className="row">
-            <Col sm="8">
-              <h2>{title}</h2>
-              <p>{text}</p>
-              <a href="/search"><button className="findmebutton"><img className="buttonlogo" src={logo}/>Find A Pet</button></a>
-            </Col>
-          </Row>
-        </Container>
-      )
-    };
+const HeroSection = ({ img, title, text }) => {
+  const background = {
+    backgroundImage: `url(${img})`,
+  };
+
+  return (
+    <Container className="container" style={background}>
+      <Row className="row">
+        <Col sm="8">
+          <h2>{title}</h2>
+          <p>{text}</p>
+          <a href="/search">
+            <button className="findmebutton">
+              <img className="buttonlogo" src={logo} />
+              Find A Pet
+            </button>
+          </a>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default HeroSection;
--- src/components/HeroSection/HeroSection.test.js
+++ src/components/HeroSection/HeroSection.test.js
@@ -2,11 +2,7 @@ import HeroSection from "./HeroSection.js";
 import React from "react";
 import renderer from "react-test-renderer";
 
-
-
-it('renders correctly', () => {
-    const tree = renderer
-      .create(<HeroSection />)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+it("renders correctly", () => {
+  const tree = renderer.create(<HeroSection />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
