85b163edcaafc5d8cf0c556b74139a442c61ef68
ayse8888 <aysebasar@yahoo.com>
2021-08-11 23:09:35 +0300

    Saving files before refreshing line endings

--- src/components/AdoptionProcess/ProcessContainer.js
+++ src/components/AdoptionProcess/ProcessContainer.js
@@ -1,22 +1,19 @@
 import React from "react";
 import ProcessItems from "./ProcessItems";
 import { Container, Row, Col } from "react-bootstrap";
-import { adoptionProcess } from "./data";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const ProcessContainer = () => {
-  // here import of adoptionProcess is coming from data.js file.
-  // this will be imported to app.js or wherever this component is rendered and then will come here as props
-  // but since i have to change the app to default, i will delete data importation from app for now
-  // and will import here because if i dont, test is giving an "map is undefined error"
-  // in the future the data will be imported as props to this component from wherever it rendered.
-
+const ProcessContainer = ({ 
+  adoptionProcess, 
+  sectionTitle 
+}) => {
   return (
     <Container>
       <Row className="adoptionContainer py-5">
         <Col sm={12}>
-          <h1>Pet Adoption Process</h1>
+          <h1>{sectionTitle.title}</h1>
           <Row className="mt-5 items">
-            {adoptionProcess.map((process) => (
+            {adoptionProcess && adoptionProcess.map((process) => (
               <ProcessItems
                 key={process.id}
                 img={process.img}
--- src/components/AdoptionProcess/processcontainer.test.js
+++ src/components/AdoptionProcess/processcontainer.test.js
@@ -1,8 +1,13 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import ProcessContainer from "./ProcessContainer.js";
+import {adoptionProcess,sectionTitle} from "../data/data"
 
 it("renders correctly", () => {
-  const tree = renderer.create(<ProcessContainer />).toJSON();
+  const tree = renderer.create(
+    <ProcessContainer 
+      adoptionProcess={adoptionProcess} 
+      sectionTitle={sectionTitle}
+    />).toJSON();
   expect(tree).toMatchSnapshot();
 });
