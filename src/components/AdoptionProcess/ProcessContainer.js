6a355038fb58cb482bcbd4bf5ced7c789d11ca47
ayse8888 <aysebasar@yahoo.com>
2021-08-12 18:47:25 +0300

    yarn run fix

--- src/components/AdoptionProcess/ProcessContainer.js
+++ src/components/AdoptionProcess/ProcessContainer.js
@@ -1,27 +1,25 @@
 import React from "react";
 import ProcessItems from "./ProcessItems";
 import { Container, Row, Col } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const ProcessContainer = ({ 
-  adoptionProcess, 
-  sectionTitle 
-}) => {
+const ProcessContainer = ({ adoptionProcess, sectionTitle }) => {
   return (
     <Container>
       <Row className="adoptionContainer py-5">
         <Col sm={12}>
           <h1>{sectionTitle.title}</h1>
           <Row className="mt-5 items">
-            {adoptionProcess && adoptionProcess.map((process) => (
-              <ProcessItems
-                key={process.id}
-                img={process.img}
-                text={process.text}
-                firstDescription={process.firstDescription}
-                secondDescription={process.secondDescription}
-              />
-            ))}
+            {adoptionProcess &&
+              adoptionProcess.map((process) => (
+                <ProcessItems
+                  key={process.id}
+                  img={process.img}
+                  text={process.text}
+                  firstDescription={process.firstDescription}
+                  secondDescription={process.secondDescription}
+                />
+              ))}
           </Row>
         </Col>
       </Row>
