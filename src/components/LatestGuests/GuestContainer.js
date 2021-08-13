afc99b7df35cff9d8885506366ee0436a41963fd
ayse8888 <aysebasar@yahoo.com>
2021-08-13 15:36:26 +0300

    Build: add Latest Guests Component

---
+++ src/components/LatestGuests/GuestContainer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import GuestItems from "./GuestItems";
+import { Container, Row, Col } from "react-bootstrap";
+import "./LatestGuests.css";
+
+const GuestContainer = ({ 
+  guests, 
+  guestsTitle 
+}) => {
+  return (
+    <Container>
+      <Row className="guestsContainer py-5">
+        <Col sm={12}>
+          <h1>{guestsTitle.title}</h1>
+          <h6>{guestsTitle.subTitle}</h6>
+          <Row className="mt-5 items">
+            {guests &&
+              guests.map((guest) => (
+                <GuestItems
+                  key={guest.id}
+                  img={guest.img}
+                  buttonText={guest.buttonText}
+                />
+              ))}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default GuestContainer;
