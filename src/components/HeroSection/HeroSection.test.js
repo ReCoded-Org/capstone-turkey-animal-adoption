0c47b9838b3cf04e962fdde09e5f9541c7014e0a
mustafaguldag <guldag.mustafaa@gmail.com>
2021-08-13 13:49:10 +0300

    Fix: test file

--- src/components/HeroSection/HeroSection.test.js
+++ src/components/HeroSection/HeroSection.test.js
@@ -2,7 +2,24 @@ import HeroSection from "./HeroSection.js";
 import React from "react";
 import renderer from "react-test-renderer";
 
+const data = {
+  img:
+    "https://ggsc.s3.amazonaws.com/images/uploads/The_Science-Backed_Benefits_of_Being_a_Dog_Owner.jpg",
+  title: "Adopt Please",
+  text: "We are non-profit organization",
+  side: "left",
+};
+
 it("renders correctly", () => {
-  const tree = renderer.create(<HeroSection />).toJSON();
+  const tree = renderer
+    .create(
+      <HeroSection
+        img={data.img}
+        title={data.title}
+        text={data.text}
+        side={data.side}
+      />
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
