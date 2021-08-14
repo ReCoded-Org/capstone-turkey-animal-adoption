249781760eb57a08ad54c6c706e4ab1efcee7599
mustafaguldag <81811326+mustafaguldag@users.noreply.github.com>
2021-08-14 19:40:30 +0300

    Hero section component (#11)
    * Build: Add hero-section
    
    * Build: add hero-section-component
    
    * Test: fixed the test
    
    * Build: add function
    
    * Test: yarn fix
    
    * Fix: test file

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../../images/buttonlogo.png";
+import "./HeroSection.css";
+
+const HeroSection = ({ img, title, text, side }) => {
+  const background = {
+    backgroundImage: `url(${img})`,
+  };
+
+  const changeLeftOrRight = () => {
+    if (side === "left") {
+      return "containerForLeft";
+    } else {
+      return "containerForRight";
+    }
+  };
+
+  return (
+    <Container className={changeLeftOrRight()} style={background}>
+      <Row>
+        <Col sm="8">
+          <h2>{title}</h2>
+          <p>{text}</p>
+          <a href="/search">
+            <button className="findMeButton">
+              <img className="buttonLogo" src={logo} />
+              Find A Pet
+            </button>
+          </a>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default HeroSection;
