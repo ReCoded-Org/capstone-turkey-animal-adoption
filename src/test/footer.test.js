32eca2a2107d503edcf5c2f1ea4a0030aeb579ea
yazjimanar <m.gmail.com>
2021-08-16 20:52:44 +0300

    solving the conflict

--- src/test/footer.test.js
+++ src/test/footer.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import Footer from "./footer";
+import Footer from "../Component/Footer/footer";
 
 it("renders correctly", () => {
   const tree = renderer.create(<Footer />).toJSON();
