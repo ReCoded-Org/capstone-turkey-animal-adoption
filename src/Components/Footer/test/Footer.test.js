12e3f1835c55d9d3f32ef78d2856fe4e64670a85
yazjimanar <m.gmail.com>
2021-08-20 21:40:33 +0300

    fix

--- src/Components/Footer/test/Footer.test.js
+++ src/Components/Footer/test/Footer.test.js
@@ -1,8 +1,15 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { BrowserRouter as Router } from "react-router-dom";
 import Footer from "../footer";
 
 it("renders correctly", () => {
-  const tree = renderer.create(<Footer />).toJSON();
+  const tree = renderer
+    .create(
+      <Router>
+        <Footer />
+      </Router>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
