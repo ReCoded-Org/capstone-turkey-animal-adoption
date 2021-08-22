f2dd8800d953ea84c656adad24c320a46784cc79
Rahaf Shora <rahaf.it@gmail.com>
2021-08-23 02:38:53 +0300

    Fix : another way to fix test

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Navbar, Nav, Button, Form } from "react-bootstrap";
 import logo from "../../images/logo.png";
-import Login from "../Login/Login";
+import Login from "../Login/Login.js";
 import "./NavBar.css";
 
 function NavBar() {
@@ -15,7 +15,7 @@ function NavBar() {
             <img
               src={logo}
               height="50"
-              alt="Our Furry Friend"
+              alt="Furry Friend"
               className="logoHeader"
             />
           </NavLink>
