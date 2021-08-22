8a0dda3474d78e8b3fd9aa26a2a43f58f7795e8a
yazjimanar <m.gmail.com>
2021-08-22 23:22:04 +0300

    Delete the previous footer branch and add a new one

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from "./pages/homepage";
 import NavBar from "./components/NavBar/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Footer from "./Components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
