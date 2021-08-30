3c0197c08459f898bc2b5d75dfd29bc8b6506eba
ayse8888 <aysebasar@yahoo.com>
2021-08-30 16:55:35 +0300

    Build:add Blog Component

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import BlogDetails from "./BlogDetails";
+import { Container, Row, Col, Spinner } from "react-bootstrap";
+
+const Blog = () => {
+  const [allPosts, setAllPosts] = useState([]);
+
+  const getAllPosts = async () => {
+    const data = await fetch(
+      "https://public-api.wordpress.com/wp/v2/sites/ecommerce458750409.wordpress.com/posts"
+    );
+
+    const posts = await data.json();
+    console.log(posts);
+    setAllPosts(posts);
+  };
+  useEffect(() => {
+    getAllPosts();
+  }, []);
+  return allPosts.length > 0 ? (
+    <BlogDetails blogContent={allPosts} />
+  ) : (
+    <Container>
+      <Row>
+        <Col>
+          <Spinner animation="grow" />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Blog;
