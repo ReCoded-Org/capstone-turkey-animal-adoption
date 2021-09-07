edab2996d3282e5725f9998d37f3981e876b2615
Rahaf Shora <rahaf.it@gmail.com>
2021-09-07 23:43:35 +0300

    Adopt for logged in users only

--- src/components/Search/SearchResults.js
+++ src/components/Search/SearchResults.js
@@ -5,11 +5,48 @@ import { BsGeoAlt } from "react-icons/bs";
 import { FaPaw } from "react-icons/fa";
 import AdoptionFormModal from "./AdoptionFormModal";
 import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+import Login from "../Login/Login";
+import { useLocalStorage } from "../PersonalDetails/useLocalStorage";
+import * as emailjs from "emailjs-com";
 
 const SearchResults = ({ img, id, location, age, breed, gender, name }) => {
-  const [modalShow, setModalShow] = useState(false);
-
   const { t } = useTranslation();
+  const [modalShow, setModalShow] = useState(false);
+  const [LoginShown, isLoginShown] = useState(false);
+  const [message, setMessage] = useState("");
+  const [adoptButton, setAdoptButton] = useState(t("searchPage.button"));
+  const phone = useLocalStorage("phone", "");
+  const city = useLocalStorage("city", "");
+  const user = useSelector((state) => state.user);
+
+  const sendMail = () => {
+    setMessage("");
+    setAdoptButton("Loading ...");
+    const emailParams = {
+      from_name: user.name,
+      from_phone: phone,
+      from_email: user.email,
+      from_city: city,
+      from_petname: name,
+      from_petbreed: breed,
+    };
+    emailjs
+      .send(
+        "service_gmz8yep",
+        "template_xfkp32m",
+        emailParams,
+        "user_kzsnySDvDdrtgNCu03zXF"
+      )
+      .then((result) => {
+        setMessage("your adoption application was sent to admins");
+        setAdoptButton(t("searchPage.button"));
+      })
+      .catch((error) => {
+        setMessage("There was an error in sending your application");
+        setAdoptButton(t("searchPage.button"));
+      });
+  };
 
   return (
     <>
@@ -21,15 +58,28 @@ const SearchResults = ({ img, id, location, age, breed, gender, name }) => {
               <div className="firstDiv">
                 <p>{breed}</p>
                 <p>{name}</p>
-                <Button
-                  variant="primary"
-                  className="shadow-none"
-                  id="adoptButton"
-                  onClick={() => setModalShow(true)}
-                >
-                  <FaPaw className="pawIcon" />
-                  {t("searchPage.button")}
-                </Button>
+                {!user && (
+                  <Button
+                    variant="primary"
+                    className="shadow-none"
+                    id="adoptButton"
+                    onClick={() => isLoginShown(true)}
+                  >
+                    <FaPaw className="pawIcon" />
+                    Login To Adopt
+                  </Button>
+                )}
+                {user && (
+                  <Button
+                    variant="primary"
+                    className="shadow-none"
+                    id="adoptButton"
+                    onClick={sendMail}
+                  >
+                    <FaPaw className="pawIcon" />
+                    {adoptButton}
+                  </Button>
+                )}
               </div>
               <div className="secondDiv">
                 <p>
@@ -40,10 +90,12 @@ const SearchResults = ({ img, id, location, age, breed, gender, name }) => {
                 </p>
               </div>
             </div>
+            <p class="mt-3 mb-0">{message}</p>
           </Card.Body>
         </Card>
       </Col>
       <AdoptionFormModal show={modalShow} onHide={() => setModalShow(false)} />
+      <Login show={LoginShown} isModalShown={isLoginShown} />
     </>
   );
 };
--- src/components/Search/search.test.js
+++ src/components/Search/search.test.js
@@ -2,8 +2,16 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Search from "./Search.js";
 import { searchGuests } from "./data";
+import store from "../../store/store";
+import { Provider } from "react-redux";
 
 it("renders Search correctly", () => {
-  const tree = renderer.create(<Search searchGuests={searchGuests} />).toJSON();
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <Search searchGuests={searchGuests} />
+      </Provider>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
